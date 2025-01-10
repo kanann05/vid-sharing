6ac205c2d11786302ff90b0a11429b3c12c7e7ed
kanann05 <148896349+kanann05@users.noreply.github.com>
2025-01-11 01:19:41 +0530

    second

---
+++ client/src/data.js
@@ -0,0 +1,5 @@
+import React from "react";
+let data = [{
+
+}];
+export default data;
---
+++ client/src/home.js
@@ -0,0 +1,131 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate, useNavigate, Form } from 'react-router-dom';
+import data from './data.js'
+import axios from 'axios';
+
+function Home({setLoggedin }) {
+  let navigate = useNavigate();
+  let [upvis, setUpvis] = useState(false);
+  let [fn, setFn] = useState("");
+  let [th, setTh] = useState("");
+  let [file, setFile] = useState(null);
+  let constfile = file;
+  useEffect(() => {
+    constfile = file;
+  }, [file])
+  useEffect(() => {
+
+    const navUploadDb = document.querySelector('.nav-upload-db');
+    if (upvis) {
+      navUploadDb.classList.add('visible');
+    
+    } else {
+      navUploadDb.classList.remove('visible');
+      
+    }
+  }, [upvis]);
+  return (<div className = "home" style = {{minHeight : "100vh", display : "flex", alignItems:"center", flexDirection : "column"}}>
+    
+    {/* <div className = "nav-bar">
+        >
+    </div> */}
+
+  
+  <div className = "nav-bar">
+  
+
+    <div className = "nav-search-bar">
+        <input style = {{backgroundColor : "transparent"}} className = "search-bar"></input>
+
+        <div className = "search-img"
+        
+          style = {{
+            height : "100%",
+            width : "max(60px, 7.5%)",
+            position : "relative",
+            display:"flex",
+            alignItems:"center",
+            justifyContent : "center",
+
+          }}
+          
+        >
+          <img style = {{filter : "invert()", opacity : "0.7", width : "30%"}} src = "src-img.png" alt = "search img"/>
+        </div>
+
+    </div>
+    <div className = "nav-buttons" style = {{
+      display : "flex",
+      flexDirection : "row",
+      justifyContent : "space-between",
+      width : "30%"
+    }}>
+      <div style = {{position : "relative"}}>
+      <button className="nav-upload" onClick = {() => {setUpvis(!upvis)}}>
+        <div style= {{margin : "0", display:"flex", flexDirection:"row", justifyContent : "center", alignItems:"center"}}>
+        <img className = "upload-img" style =  {{
+          display : "flex", justifyContent : "center"
+        }}
+            src="upload-img.png"
+            alt="upload-icon"
+        /> 
+        <p>
+        Upload </p>
+        </div>
+    </button>
+    <div className = "nav-upload-db" >
+      
+      <input placeholder = "Folder name" value = {fn} onChange ={(e) => {
+        setFn(e.target.value);
+      }}></input>
+      <p style = {{color : "white", margin : "-5px 0 -5px 0"}}>------------------------------</p>
+      <input value = {th} placeholder = "Thumbnail link / upload" onChange={(e) => {
+        setTh(e.target.value)
+      }}>
+        
+      </input>
+      <input type ="file" className = "add-folder-input"style = {{marginTop : "-5px", width:"70%", backgroundColor : "transparent", color : "beige"}} onChange={(e)=>{
+        setFile(e.target.files[0]);
+      }}></input>
+      {/* <button style = {{width:"30%", height : "25px", marginTop : "-7px", marginBottom:"6px"}}>Upload</button> */}
+      <p style = {{color : "white", margin : "-10px 0 -5px 0"}}>------------------------------</p>
+      <button style = {{backgroundColor : "rgba(0,0,0,0.4)"}} onClick = {async (e)=>{
+        if(constfile != null && th != "") {
+          alert("either enter url or upload the image");
+          return;
+        }
+        e.preventDefault();
+        let formdata = new FormData();
+        formdata.append('img-file', constfile);
+        formdata.append('username', localStorage.getItem('username'))
+        try {
+          const res = await axios.post('/uploadimg', formdata, {
+            headers : {'content-type' : 'multipart/form-data'},
+          });
+          console.log(res.data)
+          window.location.reload()
+        }
+        catch(error) {
+          alert('folder img upload failed')
+        }
+      }}>Add</button>
+    </div>
+    </div>
+
+      <button className = "log-out" onClick ={()=> {
+          localStorage.setItem("accessToken", "");
+          localStorage.setItem("username", "")
+          setLoggedin(false);
+          
+      }}>
+        <img className = "log-out-img" src = "upload-img.png" style = {{height : "100%", aspectRatio : "1/1", transform : "rotate(270deg)", filter : "invert()", opacity : "0.7"}}/>
+        <p>
+          Log out</p>
+      </button> 
+    </div>
+  </div>
+  <div style={{ color: "white" }}>hello.</div></div>);
+}
+export default Home;
